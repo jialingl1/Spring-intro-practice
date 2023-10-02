3967d12d51e805d9c4a5c85674ddf8874a318952
Jialing Liu <jialingl.liu@gmail.com>
2023-10-02 13:01:50 -0400

    added thymeleaf dependency and a form template.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,22 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
+//@ResponseBody
 public class HelloController {
 
     // Handles requests at path /hello
     @GetMapping("hello")
+    @ResponseBody
     public String hello() {
         return "Hello, Spring!";
     }
 
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hola")
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
 
@@ -27,6 +30,7 @@ public class HelloController {
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
+    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
@@ -34,14 +38,7 @@ public class HelloController {
     //Handles requests using a form
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hola' method='post'>" + //submit a request to /hola (reusar lo de arriba)
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Saludame'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
 }
